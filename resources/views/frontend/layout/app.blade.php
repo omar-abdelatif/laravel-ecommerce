71c5648c00a8a20b70c2f5a3e5f00166a105e712
omar-abdelatif <84409576+omar-abdelatif@users.noreply.github.com>
2023-11-11 21:36:29 +0200

    update

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:type" content="" />
     <meta property="og:url" content="" />
     <meta property="og:image" content="" />
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/imgs/icons/favicon.svg')}}" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/frontend/icons/favicon.svg')}}" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('assets/frontend/css/plugins/animate.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/frontend/css/main.css')}}">
@@ -28,17 +28,38 @@
             <div class="preloader d-flex align-items-center justify-content-center">
                 <div class="preloader-inner position-relative">
                     <div class="text-center">
-                        <img src="{{asset('assets/imgs/preloader/loading.gif')}}" alt="" />
+                        <img src="{{asset('assets/frontend/imgs/preloader/loading.gif')}}" alt="" />
                     </div>
                 </div>
             </div>
         </div>
     </main>
-    <script src="{{asset('assets/js/vendor/modernizr-3.6.0.min.js')}}"></script>
-    <script src="{{asset('assets/js/vendor/jquery-3.6.0.min.js')}}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="{{asset('assets/js/plugins/scrollup.js')}}"></script>
-    <script src="{{asset('assets/js/main.js')}}"></script>
-    <script src="{{asset('assets/js/shop.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/vendor/modernizr-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/vendor/jquery-3.6.0.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/vendor/jquery-migrate-3.3.0.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/vendor/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/wow.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/scrollup.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/perfect-scrollbar.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/select2.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/magnific-popup.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/waypoints.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/counterup.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/isotope.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/jquery.countdown.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/images-loaded.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/custom-parallax.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/jquery.elevateZoom.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/jquery.theia.sticky.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/jquery-ui.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/leaflet.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/slick.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/slider-range.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/jquery.vticker-min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/jquery.syotimer.min.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/plugins/TweenMax.min.js')}}"></script>
+    <!-- Main JS -->
+    <script src="{{asset('assets/frontend/js/main.js')}}"></script>
+    <script src="{{asset('assets/frontend/js/shop.js')}}"></script>
 </body>
 </html>
